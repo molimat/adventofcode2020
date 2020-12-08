ffd6f75c1e28d08a15348cd899e1c12aece46072
Matheus Molin de Andrade <molimat@pop-os.localdomain>
2020-12-08 17:50:16 +0100

    Dia 7 nao finalizado

---
+++ 7/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type bag struct {
+	name   string
+	inside map[string]int
+}
+
+func main() {
+	fileData := readFile("data")
+	allRelations := getHierarchy(fileData)
+	fathersMap := make(map[string]int)
+	total := countPaths(allRelations, "shiny gold", fathersMap)
+	fmt.Println(len(total))
+	//	fmt.Printf("%+v", total)
+
+}
+
+func countPaths(b []bag, s string, m map[string]int) map[string]int {
+	//s is the bag willing to be counted
+
+	fathers, _ := getFathers(b, s)
+	for _, father := range fathers {
+		m[father] = 1
+		m = countPaths(b, father[:len(father)-5], m)
+	}
+	return m
+
+}
+
+func getFathers(b []bag, s string) ([]string, int) {
+	var fathers []string
+	counter := 0
+	for _, bag := range b {
+		if len(bag.inside) != 0 {
+			for key := range bag.inside {
+				if strings.Contains(key, s) {
+					fathers = append(fathers, bag.name) //dad
+					counter++
+				}
+			}
+		}
+	}
+	return fathers, counter
+}
+
+func getChildren(b []bag, s string) []string {
+	var children []string
+	for _, bag := range b {
+		if strings.Contains(bag.name, s) && len(bag.inside) != 0 {
+			for key := range bag.inside {
+				children = append(children, key)
+			}
+		}
+	}
+	return children
+}
+
+func getHierarchy(s []string) []bag {
+	relations := []bag{}
+	for _, value := range s {
+		name, children := getOrder(value)
+		relations = append(relations, bag{name, children})
+	}
+	return relations
+}
+
+func getOrder(s string) (string, map[string]int) { // return dad bag and children
+	i := strings.Index(s, " contain")
+	dad := string(s[:i])
+	children := string(s[i+8:])
+	childrenMap := make(map[string]int)
+
+	if strings.Contains(s, "no other bags") {
+		return "", childrenMap
+	}
+
+	for strings.ContainsAny(children, ",") || strings.ContainsAny(children, ".") {
+		if strings.Contains(children, "bags,") {
+			k := strings.Index(children, ",")
+			childrenMap[string(children[3:k-5])], _ = strconv.Atoi(string(children[1]))
+			//fmt.Println(string(children[3:k]))
+			children = string(children[k+1:])
+		} else if strings.Contains(children, "bag,") {
+			k := strings.Index(children, ",")
+			childrenMap[string(children[3:(k-4)])], _ = strconv.Atoi(string(children[1]))
+			//fmt.Println(string(children[3:k]))
+			children = string(children[k+1:])
+		} else if strings.Contains(children, "bags.") {
+			k := strings.Index(children, ".")
+			childrenMap[string(children[3:k-5])], _ = strconv.Atoi(string(children[1]))
+			children = ""
+		} else {
+			k := strings.Index(children, ".")
+			childrenMap[string(children[3:k-4])], _ = strconv.Atoi(string(children[1]))
+			children = ""
+		}
+	}
+
+	return dad, childrenMap
+}
+
+func readFile(filename string) []string {
+	file, _ := os.Open(filename)
+
+	scanner := bufio.NewScanner(file)
+
+	var fileData []string
+
+	for scanner.Scan() {
+		currentLine := scanner.Text()
+		fileData = append(fileData, string(currentLine))
+	}
+
+	return fileData
+}
